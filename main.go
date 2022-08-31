17b4cb5da3c83a978d4eabe2c046f6741de8a997
Boyan Deshev <grievous@abv.bg>
2022-08-31 15:00:58 +0300

    code with error

--- main.go
+++ main.go
@@ -10,51 +10,68 @@ import (
 const paddleHeight = 4
 const paddleSymbol = 0x2588
 
-func PrintString(s tcell.Screen, col, row int, str string) {
+type Paddle struct {
+	row, col, width, height int
+}
+
+var screen tcell.Screen
+var player1 *Paddle
+var player2 *Paddle
+
+func PrintString(col, row int, str string) {
 	for _, c := range str {
-		s.SetContent(col, row, c, nil, tcell.StyleDefault)
+		screen.SetContent(col, row, c, nil, tcell.StyleDefault)
 		col += 1
 	}
 }
 
-func Print(s tcell.Screen, row, col, width, height int, ch rune) {
+func Print(row, col, width, height int, ch rune) {
 	for r := 0; r < height; r++ {
 		for c := 0; c < width; c++ {
-			s.SetContent(col+c, row+r, ch, nil, tcell.StyleDefault)
+			screen.SetContent(col+c, row+r, ch, nil, tcell.StyleDefault)
 		}
 	}
 }
 
-func displayPaddles(screen tcell.Screen) {
+func displayPaddles() {
 	screen.Clear()
 	width, height := screen.Size()
 	paddleStart := height/2 - paddleHeight/2
-	Print(screen, paddleStart, 0, 1, paddleHeight, paddleSymbol)
-	Print(screen, paddleStart, width-1, 1, paddleHeight, paddleSymbol)
+	Print(paddleStart, 0, 1, paddleHeight, paddleSymbol)
+	Print(paddleStart, width-1, 1, paddleHeight, paddleSymbol)
 	screen.Show()
 }
 
 func main() {
-	screen := InitScreen()
+	InitScreen()
 	for {
 		switch ev := screen.PollEvent().(type) {
 		case *tcell.EventKey:
 			if ev.Key() == tcell.KeyESC {
 				screen.Fini()
 				os.Exit(0)
+			} else if ev.Key() == tcell.KeyUp {
+				player1.row++
+			} else if ev.Key() == tcell.KeyDown {
+				player1.row--
+			} else if ev.Rune() == 'w' {
+				player2.row++
+			} else if ev.Rune() == 's' {
+				player2.row--
 			}
 		}
 	}
 }
 
-func InitScreen() tcell.Screen {
-	screen, err := tcell.NewScreen()
+func InitScreen() {
+	var err error
+	screen, err = tcell.NewScreen()
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "%v\n", err)
 		os.Exit(1)
 	}
-	if e := screen.Init(); e != nil {
-		fmt.Fprintf(os.Stderr, "%v\n", e)
+	if err := screen.Init(); err != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", err)
 		os.Exit(1)
 	}
 
@@ -62,8 +79,16 @@ func InitScreen() tcell.Screen {
 		Background(tcell.ColorBlack).
 		Foreground(tcell.ColorWhite)
 	screen.SetStyle(defStyle)
+}
 
-	displayPaddles(screen)
+func InitGameState() {
+	width, height := screen.Size()
+	paddleStart := height/2 - paddleHeight/2
 
-	return screen
+	player1 = &Paddle{
+		row: paddleStart, col: 0, width: 1, height: paddleHeight,
+	}
+	player2 = &Paddle{
+		row: paddleStart, col: width - 1, width: 1, height: paddleHeight,
+	}
 }
